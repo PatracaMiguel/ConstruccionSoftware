a0c87222edcb5e5ca9062b92ba722fb3820cb421
Miguel <miguelangelpatracalagunes@gmail.com>
2025-10-15 12:34:57 -0600

    dc

--- InterfaceSegregation/SegregacionInterfaces/obj/Debug/net9.0/Exposicion22.AssemblyInfo.cs
+++ InterfaceSegregation/SegregacionInterfaces/obj/Debug/net9.0/Exposicion22.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exposicion22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+869165dc23d784f493ba7927eac90c3bf6b3b559")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8e42c146b2e572a26aa63349b96c96d50e0843b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exposicion22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exposicion22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
